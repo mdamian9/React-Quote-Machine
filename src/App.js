ac8807907f6a0de2e05d19555b26cacac80c5566
mdamian9 <m.damian9@hotmail.com>
2019-11-13 00:00:54 -0800

    Installed axios and changed App component to a class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <div className='text-center'>
-        App
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: 'quote'
+    };
+  };
+
+  getNewQuote = () => {
+    console.log('get new quote');
+  };
+
+  render = () => {
+    return (
+      <div>
+        <div className='container'>
+          <div className='row'>
+            <div className='col text-center'>
+              App
+              <div id='quote-box'>
+                <div id='text'>
+                  {this.state.quote}
+                </div>
+                <br />
+                <div id='author'>
+
+                </div>
+                <br />
+                <button onClick={this.getNewQuote} id='new-quote' className='btn btn-danger'>New Quote</button>
+                <br />
+                <a href='https://twitter.com/' id='tweet-quote'>Tweet Quote</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default App;
